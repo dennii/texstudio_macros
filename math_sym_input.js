0e27c94bf345089e343b38fe7c89c35492e24283
Dennis Barrett <dennii@users.noreply.github.com>
2017-11-01 15:32:20 +0200

    Update math_sym_input.js
    Fixed tab/space issues

--- math_sym_input.js
+++ math_sym_input.js
@@ -53,24 +53,24 @@ var cn = cursor.columnNumber()
 if (cn > 1) {
   var s = editor.document().cursor(ln,cn-3,ln,cn)
   var t = s.selectedText()
-  var i = t.indexOf("¦");
+  var i = t.indexOf("Â¦");
   
-	var key = t.slice(i+1)
+  var key = t.slice(i+1)
   if (i != -1 && ((key.length == 1 && key.toLowerCase() in dict) 
-							 || (key.length  > 1 && key in dict))) {
+	       || (key.length  > 1 && key in dict))) {
     for (var k = 0; k < t.length-i; k++) {
       cursor.deletePreviousChar()
     }
-		if (key.length == 1 && /[A-Z]/.test(key)) {
-			var sub = dict[key.toLowerCase()]
-			editor.write("\\" + sub[0].toUpperCase() + sub.slice(1))
-		} else {
-			editor.write("\\" + dict[key])
-		}
+    if (key.length == 1 && /[A-Z]/.test(key)) {
+      var sub = dict[key.toLowerCase()]
+      editor.write("\\" + sub[0].toUpperCase() + sub.slice(1))
+    } else {
+      editor.write("\\" + dict[key])
+    }
   } else {
-    editor.write("¦");
+    editor.write("Â¦");
   }
 } else {
-  editor.write("¦")
+  editor.write("Â¦")
 }
 
